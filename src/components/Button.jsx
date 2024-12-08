854e41f7d6a2bf6106a921268db438f457f4b459
Imam Darmawan <79206804+imam-darmawan@users.noreply.github.com>
2024-12-08 17:31:06 +0700

    Improve border and border animation on button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,36 +4,25 @@ import PropTypes from "prop-types";
 import { useRef } from "react";
 
 const Button = ({ text, url, shortened }) => {
-  const { contextSafe } = useGSAP();
+  const buttonContainer = useRef();
+  const borderAnimation = useRef();
 
-  const border = useRef();
-
-  const borderMask = {
-    position: "0%",
-    get mask() {
-      return `
-        linear-gradient(to right, black calc(${this.position} + 8px), #0000 0),
-        linear-gradient(to left, black calc(${this.position} + 8px), #0000 0)
-    `;
-    },
-  };
-
-  const borderAnimation = gsap.to(borderMask, {
-    position: "50.1% - 8px",
-    paused: true,
-    ease: "expo.inOut",
-    duration: 0.8,
-    onUpdate: () => {
-      border.current.style.mask = borderMask.mask;
-    },
+  const { contextSafe } = useGSAP(() => {
+    borderAnimation.current = gsap.to(buttonContainer.current, {
+      "--parenthesis-size": "50.1% + 0px",
+      paused: true,
+      ease: "expo.inOut",
+      duration: 0.8,
+    });
   });
 
   return (
     <a
       href={url ?? "#"}
-      className="group relative inline-flex h-9 items-center justify-center text-nowrap px-4 text-xl uppercase max-sm:text-lg"
-      onMouseEnter={contextSafe(() => borderAnimation.play())}
-      onMouseLeave={contextSafe(() => borderAnimation.reverse())}
+      className="parenthesize group inline-flex h-9 items-center justify-center text-nowrap px-4 text-xl uppercase max-sm:text-lg"
+      onMouseEnter={contextSafe(() => borderAnimation.current.play())}
+      onMouseLeave={contextSafe(() => borderAnimation.current.reverse())}
+      ref={buttonContainer}
     >
       {/* Text */}
       {shortened ? (
@@ -46,13 +35,6 @@ const Button = ({ text, url, shortened }) => {
       ) : (
         <div>{text}</div>
       )}
-
-      {/* Border */}
-      <div
-        className="absolute inset-0 rounded-full border border-white"
-        style={{ mask: borderMask.mask }}
-        ref={border}
-      ></div>
     </a>
   );
 };
